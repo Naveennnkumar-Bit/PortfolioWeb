b2483ededcee52e767bbda0c8ffb7988babaaa11
Naveennnkumar-Bit <naveennnkumar.03@gmail.com>
2024-12-30 14:26:49 +0530

    all done

---
+++ data/index.ts
@@ -0,0 +1,156 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I focus on delivering actionable insights through data quality, business alignment, and clear communication.",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I transform data into actionable insights",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "ML enthusiast developing and crafting models that turn data into impactful insights",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "I’m Currently discovering the endless possibilities within the dynamic world of data science.",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Data Cleaning & Transformation",
+    des: "Data Science Job Posting on Glassdoor",
+    img: "/p1.webp",
+    iconLists: ["/python.png", "/pdn.png", "/Jupyter.png"],
+    link: "https://github.com/Naveennnkumar-Bit/Glassdoor-Data-Science-Jobs-Tranformation_Python",
+  },
+  {
+    id: 2,
+    title: "Data Analysis",
+    des: "My Youtube Watch Habits",
+    img: "/p2.webp",
+    iconLists: ["/python.png", "/pdn.png", "/matplotlib.png", "/seaborn-1.svg", "/nltk.png"],
+    link: "https://github.com/Naveennnkumar-Bit/My-YouTube-Watch-Habits",
+  },
+  {
+    id: 3,
+    title: "Data Visualization",
+    des: "Finance KPI Dashboard",
+    img: "/p3.webp",
+    iconLists: ["/powerbi.svg", "/pq.png", "/dax.png"],
+    link: "https://github.com/Naveennnkumar-Bit/Finance_KPI_Dashboard",
+  },
+  {
+    id: 4,
+    title: "SQL & Python",
+    des: "Sales and Customer Analysis",
+    img: "/p4.webp",
+    iconLists: ["/mysql.svg", "/python.png", "/anaconda.svg", "/NumPy.svg", "/pdn.png"],
+    link: "https://github.com/Naveennnkumar-Bit/SQL_Python_E-commerce_Project",
+  },
+  {
+    id: 5,
+    title: "Data Science",
+    des: "Student Performance Prediction ",
+    img: "/SPP.webp",
+    iconLists: ["/anaconda.svg", "/pdn.png","/seaborn-1.svg", "/NumPy.svg", "/sl.svg"],
+    link: "https://github.com/Naveennnkumar-Bit/mlprojects",
+  },
+    {
+    id: 6,
+    title: "Data Modelling & ETL",
+    des: "Weather Pipeline",
+    img: "/ETL.webp",
+    iconLists: ["/PostgresSQL.svg", "/AA.svg", "/Docker.svg", "/DBeaver.svg", "/python.png"],
+    link: "https://github.com/Naveennnkumar-Bit/etl-airflow-weather-pipeline",
+  }
+];
+
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Quantiums - Data Analytics Job Simulation",
+    desc: "Analyzed customer transaction data, compared trial and control stores using Python, and structured the report with the Pyramid Principle, including insights and recommendations",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Power BI Job Simulation",
+    desc: "Created a Power BI dashboard to visualize KPIs on customer demographics and gender balance at the executive level, and summarized findings and suggestions in an email to the engagement partner",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  }
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/Naveennnkumar-Bit", // GitHub URL
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+    link: "https://x.com/Naveennnkumar16", // Twitter URL
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    link: "https://www.linkedin.com/in/naveennnkumar/", // LinkedIn URL
+  },
+];
